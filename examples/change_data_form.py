bada0bc7b65940109cde492e0168a14cd1c3f0a2
kirakira666 <847868144@qq.com>
2021-05-25 17:59:59 +0800

    Update change_data_form.py

--- examples/change_data_form.py
+++ examples/change_data_form.py
@@ -102,12 +102,12 @@ for item in sortDict:
 # print(sortDict)
 
 # 生成随机语料库
-# for i in range(1000):
-#     intdex = random.randint(0, 3)
-#     Ichar = ''
-#     if i%100 == 99:
-#         Ichar = '\n'+'\n'
-#     c_file.write(l[intdex] + Ichar)
+for i in range(1000):
+    intdex = random.randint(0, 3)
+    Ichar = ''
+    if i%100 == 99:
+        Ichar = '\n'+'\n'
+    c_file.write(l[intdex] + Ichar)
 #生成随机ngram
 # for a in l:
 #     for b in l:
