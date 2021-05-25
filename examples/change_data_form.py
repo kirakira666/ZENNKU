90e72ee37985d5527b5134b9dadcdfcbbf987c76
kirakira666 <847868144@qq.com>
2021-05-25 17:56:03 +0800

    Modify change_data_form.

--- examples/change_data_form.py
+++ examples/change_data_form.py
@@ -1,12 +1,12 @@
-# import pymysql
-# db=pymysql.connect("localhost","TestUser","lyt457619283","testschema")
-# cursor=db.cursor()
-# cursor.execute("SELECT VERSION()")
-# data =cursor.fetchone()
-# print("kira:%s"%data)
-# db.close()
+
 import random
-k6_file =open("./k6.txt",'r')
+# k6_file =open("./bin/k6.txt", 'r')
+# k7_file =open("./bin/k7.txt", 'r')
+# k8_file =open("./bin/k8.txt", 'r')
+# k9_file =open("./bin/k9.txt", 'r')
+# k10_file =open("./bin/k10.txt", 'r')
+# k11_file =open("./bin/k11.txt", 'r')
+# k12_file =open("./bin/k12.txt", 'r')
 ngram_file =open("./ngram.txt",'w')
 c_file =open("./c.txt",'w')
 l = ['A','C','G','T']
@@ -14,30 +14,100 @@ resDict = {}
 # 处理数据格式
 key=[]
 value=[]
-with open("./k6.txt", "r", encoding="utf-8") as fin:
+with open("./ngram_k/k6.txt", "r", encoding="utf-8") as fin:
     for i, line in enumerate(fin):
         # print(i)
         # print(line)
         line = line.rstrip()
-        print(line)
+        # print(line)
         ngram, freq = line.split(" ")
         key.append(ngram)
         value.append(int(freq))
-print(key)
-print(value)
+
+with open("./ngram_k/k7.txt", "r", encoding="utf-8") as fin:
+    for i, line in enumerate(fin):
+        # print(i)
+        # print(line)
+        line = line.rstrip()
+        # print(line)
+        ngram, freq = line.split(" ")
+        key.append(ngram)
+        value.append(int(freq))
+
+with open("./ngram_k/k8.txt", "r", encoding="utf-8") as fin:
+    for i, line in enumerate(fin):
+        # print(i)
+        # print(line)
+        line = line.rstrip()
+        # print(line)
+        ngram, freq = line.split(" ")
+        key.append(ngram)
+        value.append(int(freq))
+
+with open("./ngram_k/k9.txt", "r", encoding="utf-8") as fin:
+    for i, line in enumerate(fin):
+        # print(i)
+        # print(line)
+        line = line.rstrip()
+        # print(line)
+        ngram, freq = line.split(" ")
+        key.append(ngram)
+        value.append(int(freq))
+
+with open("./ngram_k/k10.txt", "r", encoding="utf-8") as fin:
+    for i, line in enumerate(fin):
+        # print(i)
+        # print(line)
+        line = line.rstrip()
+        # print(line)
+        ngram, freq = line.split(" ")
+        key.append(ngram)
+        value.append(int(freq))
+
+with open("./ngram_k/k11.txt", "r", encoding="utf-8") as fin:
+    for i, line in enumerate(fin):
+        # print(i)
+        # print(line)
+        line = line.rstrip()
+        # print(line)
+        ngram, freq = line.split(" ")
+        key.append(ngram)
+        value.append(int(freq))
+
+with open("./ngram_k/k12.txt", "r", encoding="utf-8") as fin:
+    for i, line in enumerate(fin):
+        # print(i)
+        # print(line)
+        line = line.rstrip()
+        # print(line)
+        ngram, freq = line.split(" ")
+        key.append(ngram)
+        value.append(int(freq))
+
+
+# print(key)
+# print(value)
 sortDict = sorted(zip(value, key), reverse=True)
+length = len(sortDict)
+
+i = 0
 for item in sortDict:
-    print(item[0])
+    i = i+1
+    # print(str(item[1]) + ' ' + str(item[0]) + '\n')
     ngram_file.write(item[1] + ' ' + str(item[0]) + '\n')
-print(sortDict)
+    if i > 0.05*length:
+        print('select ngram：'+str(i))
+        print('total ngram：' + str(length))
+        break
+# print(sortDict)
 
 # 生成随机语料库
-for i in range(1000):
-    intdex = random.randint(0, 3)
-    Ichar = ''
-    if i%100 == 99:
-        Ichar = '\n'+'\n'
-    c_file.write(l[intdex] + Ichar)
+# for i in range(1000):
+#     intdex = random.randint(0, 3)
+#     Ichar = ''
+#     if i%100 == 99:
+#         Ichar = '\n'+'\n'
+#     c_file.write(l[intdex] + Ichar)
 #生成随机ngram
 # for a in l:
 #     for b in l:
